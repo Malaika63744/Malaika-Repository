8adc632bdbe825544dca1fd7ba9bb0633a6184df
Malaika63744 <malaikaf887@gmail.com>
2024-12-03 21:35:51 -0800

    Add files via upload

---
+++ pflab12(2).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() 
+{
+	string str = "Hello, World!";
+	
+	int length = str.length();
+	
+	cout<<"The length of the string is: "<<length<<endl;
+	
+	return 0;
+}
